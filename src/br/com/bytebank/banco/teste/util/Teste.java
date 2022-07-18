533c6bbd00f5b9e2926d58304db34fdbd653637a
Aureane Moraes <aureane.moraes@outlook.com>
2022-07-18 15:08:28 -0300

    convertendo array em list

---
+++ src/br/com/bytebank/banco/teste/util/Teste.java
@@ -0,0 +1,11 @@
+package br.com.bytebank.banco.teste.util;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Teste {
+	public static void main(String[] args) {
+		List<String> argumentos = Arrays.asList(args);
+		
+	}
+}
